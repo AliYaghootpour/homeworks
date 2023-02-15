cebaedc0fee696fbdd349df7b62da46653cf115e
AliYaghootpour <124285009+AliYaghootpour@users.noreply.github.com>
2023-02-15 19:46:13 +0330

    Add files via upload

---
+++ 2nd session.py
@@ -0,0 +1,9 @@
+num_f = "8.57"
+num_f = float(num_f)
+num_i = "3"
+num_i = int(num_i)
+div_f = num_f / num_i
+div_i = num_f // num_i
+div_r = num_f % num_i
+print(round(div_f,2))
+print(div_i,div_r)
